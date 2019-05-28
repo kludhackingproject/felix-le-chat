a62ea1869031a157ca74a026cf1fb9ca78df9bb4
klud <vincentgillibert@gmail.com>
2019-05-28 16:45:03 +0200

    [vincent] Add function ton create cart when create user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  after_create :create_cart
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -11,4 +13,8 @@ class User < ApplicationRecord
   validates :last_name, presence: true, length: { in: 0..20 }
   validates :description, presence: true, length: { in: 6..200 }
   validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "email adress please" }
+
+  def create_cart
+    Cart.create(user_id: self.id)
+  end
 end
