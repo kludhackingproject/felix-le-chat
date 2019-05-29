1155b972919372f71b8ea85de2b05714e1f30ed0
Archipo <aj.antoinejacques@gmail.com>
2019-05-29 15:33:12 +0200

    heroku

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 #ActionMailer SendGrid
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_LOGIN'],
+  :username => ENV['SENDGRID_LOGIN'],
   :password => ENV['SENDGRID_PWD'],
   :domain => 'https://projet-felix-le-chat.herokuapp.com',
   :address => 'smtp.sendgrid.net',
