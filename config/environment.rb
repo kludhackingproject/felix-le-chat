610a44457e0f064f058ee8713806c25f89ecb4ea
Archipo <aj.antoinejacques@gmail.com>
2019-05-29 19:27:57 +0200

    Mailjet

--- config/environment.rb
+++ config/environment.rb
@@ -4,10 +4,3 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-#ActionMailer MailJet
-Mailjet.configure do |config|
-  config.api_key = ENV['MJ_APIKEY_PUBLIC']
-  config.secret_key = ENV['MJ_APIKEY_PRIVATE']  
-  config.api_version = "v3.1"
-  config.end_point = "https://api.us.mailjet.com"
-end
