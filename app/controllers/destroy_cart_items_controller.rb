ebf3e4341de3c91925d0e40170e8e9075ffeac09
klud <vincentgillibert@gmail.com>
2019-05-29 18:59:00 +0200

    [Vincent] Add length items on cart and correct some bugs

--- app/controllers/destroy_cart_items_controller.rb
+++ app/controllers/destroy_cart_items_controller.rb
@@ -3,10 +3,11 @@ class DestroyCartItemsController < ApplicationController
   def create
     @cart = Cart.find(cart_id)
     @cart.items << Item.find(params[:id])
-  end 
+    redirect_to root_path
+  end
 
-  
-  
+
+
   def destroy
     @item = Cart.find(cart_id).join_table_cart_items.where(item_id: params[:id]).first
     @item.destroy
