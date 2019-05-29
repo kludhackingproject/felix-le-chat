0e9d1211383162dfa1e5c9e80fabe76caad64b56
Archipo <aj.antoinejacques@gmail.com>
2019-05-29 16:02:41 +0200

    heroku

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,7 @@ class UserMailer < ApplicationMailer
       @user = user 
   
       #on définit une variable @url qu'on utilisera dans la view d’e-mail
-      @url  = 'projet-felix-le-chat.herokuapp.com/login' 
+      @url  = 'projet-felix-le-chat.herokuapp.com' 
   
       #c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
       mail(to: @user.email, subject: 'Bienvenue chez Felix Le Chat !') 
