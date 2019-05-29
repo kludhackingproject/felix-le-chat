44b1668ecbebc7c879face4a9d2ac3e084f7cb9e
Archipo <aj.antoinejacques@gmail.com>
2019-05-29 12:07:49 +0200

    commit pushing heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # initialize action mailer to true
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
 
   # Defautl send mailer
   config.action_mailer.default_url_options = { host: 'projet-felix-le-chat.herokuapp.com' }
