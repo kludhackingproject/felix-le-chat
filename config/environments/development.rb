009129d9492b212248eb7aef76dc6f660b75da07
klud <vincentgillibert@gmail.com>
2019-05-30 20:36:26 +0200

    [vincent] Add pictures for production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,4 +66,14 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener
   # If you don't want Rails to send email, just turn to FALSE
   config.action_mailer.perform_deliveries = true
+
+  # Generate digests for assets URLs
+  config.assets.digest = true
+
+  # Disable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = false
+
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
+
 end
