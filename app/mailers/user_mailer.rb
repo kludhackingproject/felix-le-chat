e104daa0cda3700370dded984a2f41e6db926d75
Archipo <aj.antoinejacques@gmail.com>
2019-05-30 19:53:18 +0200

    [antoine] ordermail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -12,9 +12,9 @@ class UserMailer < ApplicationMailer
       mail(to: @user.email, subject: 'Bienvenue chez Felix Le Chat !') 
     end
     
-    def order_confirmation
+    def order_confirmation(user)
       
-      mail(to: current_user.email, subject: 'La commande a été reçue')
+      mail(to: user.email, subject: 'La commande a été reçue')
     end
 
     #def welcome_recap
